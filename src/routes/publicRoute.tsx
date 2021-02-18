da2d7dc449c034d59ab8f7025e2ef24f939c28af
ba-p <phuongtr3112@gmail.com>
2021-02-18 19:10:16 +0700

    Done eslint

--- src/routes/publicRoute.tsx
+++ src/routes/publicRoute.tsx
@@ -1,15 +1,22 @@
 import { Route } from "react-router-dom";
+import React from 'react';
 
+type Props = {
+    path: string,
+    exact: boolean,
+    component: React.FC
+}
 
-const publicRoute = ({ component : Component, ...rest }: any) => {
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        return <Component {...props} />;
-      }}
-    />
-  );
-};
+const publicRoute:React.FC<Props> = (props) => {
+    const {path, exact, component} = props;
+    const Component = component;
 
+    return(
+        <Route
+            path={path}
+            exact={exact}
+            render={() => <Component />}
+        />
+    )
+}
 export default publicRoute;
