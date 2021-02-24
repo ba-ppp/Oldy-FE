cb3aa742a794dbfd68216ba37403d21582309d4a
ba-p <phuongtr3112@gmail.com>
2021-02-24 19:00:27 +0700

    Done change password

--- src/components/ChangePassword/changeForget.tsx
+++ src/components/ChangePassword/changeForget.tsx
@@ -1,7 +1,6 @@
 import { Button } from 'antd';
-import { ReactComponent as NotVisible } from 'assets/images/auth/not-visible.svg';
-import { ReactComponent as Visible } from 'assets/images/auth/visible.svg';
 import logo from 'assets/images/logo/logo_192x192_w.jpg';
+import openNotificationWithIcon from 'helpers/design/notification';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import cls from './_changeForget.module.scss';
@@ -15,26 +14,45 @@ const ChangeForget: React.FC = () => {
     // form
     const { handleSubmit, register } = useForm<State>();
 
-    const [passwordType, setPasswordType] = useState('password');
-    const [rePasswordType, setRePasswordType] = useState('password');
-
-    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [password, setPassword] = useState('');
+    const [rePassword, setRePassword] = useState('');
 
     const [isEmpty, setIsEmpty] = useState(true);
-    const onChangePassword = () => {
-        setPasswordType('');
-        setRePasswordType('');
-        setPasswordVisible(true);
-        setIsEmpty(false);
+    const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setPassword(value);
+        if (value && rePassword) {
+            setIsEmpty(false);
+        } else {
+            setIsEmpty(true);
+        }
     };
-    const onChangeRePassword = () => {
-        console.log('hi');
+    const onChangeRePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setRePassword(value);
+        if (password && value) {
+            setIsEmpty(false);
+        } else {
+            setIsEmpty(true);
+        }
     };
-    const hidePassword = () => {
-        console.log('hi');
-    };
-    const onFinish = () => {
-        console.log('hi');
+    const onFinish = (values: State) => {
+        if (password.length < 6) {
+            openNotificationWithIcon(
+                'warning',
+                'Đổi mật khẩu thất bại',
+                'Mật khẩu quá ngắn'
+            );
+            return null;
+        }
+        if (password !== rePassword) {
+            openNotificationWithIcon(
+                'warning',
+                'Đổi mật khẩu thất bại',
+                'Mật khẩu không khớp'
+            );
+            return null;
+        }
     };
     return (
         <div className={cls.main}>
@@ -47,48 +65,16 @@ const ChangeForget: React.FC = () => {
                         className={cls.input}
                         placeholder="Mật khẩu mới"
                         onChange={onChangePassword}
-                        type={passwordType}
+                        type="password"
                     />
-                    <div className={cls.password}>
-                        {passwordVisible && (
-                            <Visible
-                                height={20}
-                                className={cls.icon}
-                                onClick={hidePassword}
-                            />
-                        )}
-                        {!passwordVisible && (
-                            <NotVisible
-                                height={20}
-                                className={cls.icon}
-                                onClick={hidePassword}
-                            />
-                        )}
-                    </div>
                     <input
                         name="repassword"
                         ref={register}
                         className={cls.input}
                         placeholder="Nhập lại mật khẩu"
                         onChange={onChangeRePassword}
-                        type={rePasswordType}
+                        type="password"
                     />
-                    <div className={cls.password}>
-                        {passwordVisible && (
-                            <Visible
-                                height={20}
-                                className={cls.icon}
-                                onClick={hidePassword}
-                            />
-                        )}
-                        {!passwordVisible && (
-                            <NotVisible
-                                height={20}
-                                className={cls.icon}
-                                onClick={hidePassword}
-                            />
-                        )}
-                    </div>
                     <Button
                         className={cls.button}
                         type="primary"
