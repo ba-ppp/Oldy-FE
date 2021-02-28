36de6c08a7f97d9564bfa71d351561afd794a0a9
ba-p <phuongtr3112@gmail.com>
2021-02-28 22:11:53 +0700

    Done responsive

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,8 +2,14 @@ import Header from 'components/Header';
 import React from 'react';
 import cls from './_home.module.scss';
 import Post from './Post';
+import { ReactComponent as HomeIcon } from 'assets/images/home/home.svg';
+import { ReactComponent as FindIcon } from 'assets/images/home/find.svg';
+import { ReactComponent as AddIcon } from 'assets/images/home/add.svg';
+import { ReactComponent as HeartIcon } from 'assets/images/home/heart.svg';
 
 const Home: React.FC = () => {
+    const backGround = 'https://loremflickr.com/320/240';
+
     return (
         <div>
             <Header />
@@ -12,7 +18,34 @@ const Home: React.FC = () => {
                     <Post />
                     <Post />
                 </div>
-                <div className={cls.nav}>hi</div>
+                <div className={cls.footer}>
+                    <div className={cls.nav}>
+                        <HomeIcon
+                            height={25}
+                            width={25}
+                            className={cls.nav_icon}
+                        />
+                        <FindIcon
+                            width={25}
+                            height={25}
+                            className={cls.nav_icon}
+                        />
+                        <AddIcon
+                            width={25}
+                            height={25}
+                            className={cls.nav_icon}
+                        />
+                        <HeartIcon
+                            width={25}
+                            height={25}
+                            className={cls.nav_icon}
+                        />
+                        <div
+                            className={cls.nav_icon_avt}
+                            style={{ backgroundImage: `url(${backGround})` }}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
